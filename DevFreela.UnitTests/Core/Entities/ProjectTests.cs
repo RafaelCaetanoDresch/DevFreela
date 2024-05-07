2aded85fd048416eb5b5a62b3f680276c455af68
Rafael Caetano Dresch <ti-rafael@live.com>
2024-05-06 22:57:30 -0300

    tests

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,35 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.UnitTests.Core.Entities;
+
+public class ProjectTests
+{
+    private Project project = new Project(title: "Titulo Projeto",
+                                          description: "Descrição do Projeto",
+                                          clientID: Guid.NewGuid(),
+                                          freelancerID: Guid.NewGuid(),
+                                          totalCost: 10000);
+
+    [Fact]
+    public void TestIfProjectStartWorks()
+    {
+        //Arrange
+        Assert.Equal(ProjectStatusEnum.Created, project.Status);
+        Assert.Null(project.StartedAt);
+
+        Assert.NotNull(project.Title);
+        Assert.NotEmpty(project.Title);
+
+        Assert.NotNull(project.Description);
+        Assert.NotEmpty(project.Description);
+
+        //Act
+        project.Start();
+
+        //Assert
+        Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+        Assert.NotNull(project.StartedAt);
+
+    }
+}
